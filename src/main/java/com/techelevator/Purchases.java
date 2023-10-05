d0c06d70063568a3f77152a18ae1407a9a94ea8f
Meg Lemon <meganmadalynn@gmail.com>
2023-10-05 16:30:35 -0400

    updates

--- src/main/java/com/techelevator/Purchases.java
+++ src/main/java/com/techelevator/Purchases.java
@@ -55,13 +55,13 @@ public class Purchases {
             // show available items with prices and location
             // allow the user to input the code for the item
 
-                String itemSelected = userInput.nextLine();
+
 
 //        - If the product code doesn't exist, the vending machine informs the customer and returns them
 //        to the Purchase menu.
 
                 try {
-
+                    String itemSelected = userInput.nextLine();
                 } catch (InvalidCodeInput e) {
                     // return to main menu
                     System.out.println("Sorry! That code is invalid!");
