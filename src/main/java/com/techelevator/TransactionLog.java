a7abf2f80a8f2dbb5588bc4e8e4b17907e585819
Caleb Vits <calebvits@gmail.com>
2023-10-05 16:21:31 -0400

    updates

--- src/main/java/com/techelevator/TransactionLog.java
+++ src/main/java/com/techelevator/TransactionLog.java
@@ -1,8 +1,20 @@
 package com.techelevator;
 
 import java.io.File;
+import java.io.PrintWriter;
 
 public class TransactionLog extends Report{
+
+    File outputFile = new File("src/main/java/com/techelevator/Log.txt");
+    try (PrintWriter printerWriter = new PrintWriter(outputFile)) {
+        Purchases purchases = new Purchases();
+        for (int i = 0; i < purchases.transactionLog.size(); i++) {
+            printerWriter.println(purchases.transactionLog.get(0));
+            
+        }
+    } catch (Exception e) {
+        
+    }
 
 
 }
