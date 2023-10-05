ccc5807574ad5f4a00ce19ec39ac0ff3acbeb375
Meg Lemon <meganmadalynn@gmail.com>
2023-10-05 12:22:59 -0400

    changed Item

--- src/main/java/com/techelevator/Item.java
+++ src/main/java/com/techelevator/Item.java
@@ -2,66 +2,33 @@ package com.techelevator;
 
 public class Item {
 
+    private final String GUM_MESSAGE = "Chew Chew, Yum!";
+    private final String CANDY_MESSAGE = "Munch Munch, Yum!";
+    private final String CHIP_MESSAGE = "Crunch Crunch, Yum!";
+    private final String DRINK_MESSAGE = "Glug Glug, Yum!";
     private String name;
     private double price;
     private int quantity;
-    private String message;
+    private String type;
     private String location;
 
+    
     public Item() {
     }
 
-    public Item(String name, double price, int quantity, String message, String location) {
+    public Item(String name, double price, int quantity, String type) {
         this.name = name;
         this.price = price;
         this.quantity = quantity;
-        this.message = message;
+        this.type = type;
+    }
+
+    public Item(String name, double price, int quantity, String type, String location) {
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+        this.type = type;
         this.location = location;
     }
 
-    public Item(String name, double price, int quantity) {
-        this.name = name;
-        this.price = price;
-        this.quantity = quantity;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
 }
---
+++ src/main/java/com/techelevator/MainMenu.java
@@ -0,0 +1,16 @@
+package com.techelevator;
+
+public class MainMenu {
+
+    public static void main(String[] args) {
+
+        switch (userInput) {
+            case 1:
+            case 2:
+            case 3:
+            default:
+        }
+    }
+
+
+}
